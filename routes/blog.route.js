b2db0738f40f436840df6427a23c3df892d99cb5
manuj <manujdixit04@gmail.com>
2024-03-07 17:36:49 +0530

    b3

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -7,14 +7,16 @@ const {
   updateBlog,
   deleteBlog,
 } = require("../controller/blog.ctrl");
+const { jwtMiddleware } = require("../middlewares/jwt.middleware");
+const { isAdmin } = require("../middlewares/isadmin.middleware");
 
 router.get("/", (req, res) => {
   res.send("Hello Blogs");
 });
-router.post("/create", createBlog);
-router.put("/update/:id", updateBlog);
+router.post("/create", isAdmin, createBlog);
+router.put("/update/:id", isAdmin, updateBlog);
 router.get("/get", getAllBlogs);
 router.get("/get/:id", getBlog);
-router.delete("/delete/:id", deleteBlog);
+router.delete("/delete/:id", isAdmin, deleteBlog);
 
 module.exports = router;
